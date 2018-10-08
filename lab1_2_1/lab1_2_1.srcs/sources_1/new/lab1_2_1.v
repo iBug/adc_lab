889b196b159d0d283c58e346f5345e5df7186c68
iBug <7273074+iBug@users.noreply.github.com>
2018-10-08 12:21:54 +0800

    Create lab1_2_1

---
+++ lab1_2_1/lab1_2_1.srcs/sources_1/new/lab1_2_1.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/08/2018 12:02:56 PM
+// Design Name: 
+// Module Name: lab1_2_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module lab1_2_1(
+    input [7:0] x_in,
+    output [7:0] y_out
+    );
+    assign y_out = x_in;
+endmodule
