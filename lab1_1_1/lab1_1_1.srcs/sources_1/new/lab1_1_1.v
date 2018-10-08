d6b1a9181b407f73c5af9dd3028541976e573561
iBug <7273074+iBug@users.noreply.github.com>
2018-10-08 10:25:33 +0800

    Initialize lab1_1_1

---
+++ lab1_1_1/lab1_1_1.srcs/sources_1/new/lab1_1_1.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/08/2018 10:24:35 AM
+// Design Name: 
+// Module Name: lab1_1_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module lab1_1_1(
+    input x,
+    input y,
+    input s,
+    output m
+    );
+endmodule
