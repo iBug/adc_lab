aa6a0e73e7e8773641a9157b3fc5a46bc98dcdbd
iBug <7273074+iBug@users.noreply.github.com>
2018-10-13 22:45:15 +0800

    Fancy lab2_3_1, supports subtraction

--- lab2_3_1/lab2_3_1.srcs/sources_1/new/lab2_3_1.v
+++ lab2_3_1/lab2_3_1.srcs/sources_1/new/lab2_3_1.v
@@ -29,10 +29,15 @@ module lab2_3_1(
     input sub
     );
     
-    wire [3:0] c;
-    assign co = c[3];
-    full_adder fa0(a[0], b[0], ci, s[0], c[0]);
-    full_adder fa1(a[1], b[1], c[0], s[1], c[1]);
-    full_adder fa2(a[2], b[2], c[1], s[2], c[2]);
-    full_adder fa3(a[3], b[3], c[2], s[3], c[3]);
+    wire [3:0] c, b_s, sub_ext;
+    wire ci_s, co_s;
+    assign sub_ext = {sub, sub, sub, sub};
+    assign co_s = c[3];
+    assign co = co_s ^ sub;
+    assign ci_s = ci ^ sub;
+    assign b_s = b ^ sub_ext;
+    full_adder fa0(a[0], b_s[0], ci_s, s[0], c[0]);
+    full_adder fa1(a[1], b_s[1], c[0], s[1], c[1]);
+    full_adder fa2(a[2], b_s[2], c[1], s[2], c[2]);
+    full_adder fa3(a[3], b_s[3], c[2], s[3], c[3]);
 endmodule
