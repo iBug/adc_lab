d3b01afcb01a868d08f9bb280e5af6ef8036cd3d
iBug <7273074+iBug@users.noreply.github.com>
2018-11-01 22:30:18 +0800

    Initialize lab8_1_1

---
+++ lab8_1_1/lab8_1_1.srcs/sources_1/new/lab8_1_1.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/01/2018 10:42:47 PM
+// Design Name: 
+// Module Name: lab8_1_1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module lab8_1_1(
+
+    );
+endmodule
