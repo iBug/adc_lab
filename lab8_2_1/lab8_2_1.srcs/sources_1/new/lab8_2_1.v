beb621f7c31c910eb8f2b9c52a102ac73212dcc3
iBug <7273074+iBug@users.noreply.github.com>
2018-11-14 21:15:46 +0800

    Improve lab8_2_1
    Use of a parameter
    ASYNC RESET !!!

--- lab8_2_1/lab8_2_1.srcs/sources_1/new/lab8_2_1.v
+++ lab8_2_1/lab8_2_1.srcs/sources_1/new/lab8_2_1.v
@@ -33,9 +33,8 @@ module lab8_2_1(
     input CLK100MHZ
     );
 
-    wire clk;
+    wire clk, clk_e;
     wire CLK5MHZ;
-    reg clk_e;
     integer count;
     assign clk = clk_e & en;
     clk_5MHz clk_catalog (CLK5MHZ, CLK100MHZ);
@@ -44,8 +43,8 @@ module lab8_2_1(
     wire clk0, clk1, carry0, carry1;
     wire carry_m;
 
-    assign clk0 = clk;
-    assign clk1 = (reset | carry_m) & clk;
+    assign clk0 = (clk & ~reset) | (CLK5MHZ & reset);
+    assign clk1 = (carry_m & clk & ~reset) | (CLK5MHZ & reset);
 
     // These two lines buffer the carry so it goes high *after* overflow
     D_trigger carry_mm (carry0, ~clk, carry_m),
@@ -56,18 +55,18 @@ module lab8_2_1(
     seg_manager seg_core (N0, N1, CLK5MHZ, SEG, AN);
 
     initial begin
-        clk_e <= 0;
         count <= 0;
     end
 
+    parameter CLK_CYCLE = 2500000; // Change to 5M for 1 Hz clock
+    assign clk_e = count >= CLK_CYCLE / 2;
     always @(posedge CLK5MHZ) begin
-        if (en)
+        if (en) begin
             count = count + 1;
+            if (count >= CLK_CYCLE)
+                count <= 0;
+        end
         else
             count = 0;
-        if (count >= 1250000) begin
-            count <= 0;
-            clk_e <= ~clk_e;
-        end
     end
 endmodule
