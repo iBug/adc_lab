0016ec24d9f164517e1a66a029306ec1d8230c37
iBug <7273074+iBug@users.noreply.github.com>
2018-11-16 20:53:15 +0800

    Add a "Hold" switch

--- lab9_3_1/lab9_3_1.srcs/sources_1/new/lab9_3_1.v
+++ lab9_3_1/lab9_3_1.srcs/sources_1/new/lab9_3_1.v
@@ -58,12 +58,22 @@ module lab9_3_1(
 
     assign clk = (CLK10HZ & ~Reset) | (CLK5MHZ & Reset);
 
-    wire [3:0] num0, num1, num2, num3;
+    reg [3:0] num0, num1, num2, num3;
+    wire [3:0] Q0, Q1, Q2, Q3;
     wire th0, th1, th2, th3;
-    ip_counter_10 n0 (clk, Enable, Reset, th0, num0);
-    ip_counter_10 n1 (clk, Enable & th0, Reset, th1, num1);
-    ip_counter_6 n2 (clk, Enable & th0 & th1, Reset, th2, num2);
-    ip_counter_10 n3 (clk, Enable & th0 & th1 & th2, Reset, th3, num3);
+    ip_counter_10 n0 (clk, Enable, Reset, th0, Q0);
+    ip_counter_10 n1 (clk, Enable & th0, Reset, th1, Q1);
+    ip_counter_6 n2 (clk, Enable & th0 & th1, Reset, th2, Q2);
+    ip_counter_10 n3 (clk, Enable & th0 & th1 & th2, Reset, th3, Q3);
 
     seg_manager seg (CLK5MHZ, num0, num1, num2, num3, SEG, DP, AN);
+
+    always @(Q0 or Q1 or Q2 or Q3 or Hold) begin
+        if (~Hold) begin
+            num0 <= Q0;
+            num1 <= Q1;
+            num2 <= Q2;
+            num3 <= Q3;
+        end
+    end
 endmodule
